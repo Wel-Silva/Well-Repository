048b2ebc32f84882b4455dde8d29f570d437ce70
wellsilva-ti <wellington.silva@zup.com.br>
2021-03-17 18:25:09 -0300

    terceiro commit

--- lista04/lista_04_exe20.c
+++ lista04/lista_04_exe20.c
@@ -3,4 +3,20 @@
 
 int main(){
     
+    int n, x = 0;
+
+    printf("Digite o numero de linhas que serao impressas 'Triangulo de Floyd'");
+    scanf("%d", &n);
+
+
+
+    for(int i = 0; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            x++;
+            printf("%d ", x);
+            
+        }
+        printf("\n");
+    }
+    return 0;
 }
