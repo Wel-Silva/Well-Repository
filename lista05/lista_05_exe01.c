f5665733bb9a98a07c8a7d53f30c8ec3cc45ae1d
wellsilva-ti <wellington.silva@zup.com.br>
2021-03-19 22:33:56 -0300

    atualização

--- lista05/lista_05_exe01.c
+++ lista05/lista_05_exe01.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 int main(){
 
+    int vetor [ 2 ];
+
+
+
+    printf("Entre com o numero 1: ");
+    scanf("%d", &vetor[0]);
+    printf("\nEntre com o numero 2: ");
+    scanf("%d", &vetor[1]);
+  //  printf("Entre com o número 3: ");
+   // int vetor[2] = 4;
+  //  printf("Entre com o número 4: ");
+   // int vetor[3] = 0;
+  //  printf("Entre com o número 5: ");
+   // int vetor[4] = 3;
+  //  printf("Entre com o número 6: ");
+  //  int vetor[5] = 6;
+    
+    printf("\nOs valores lidos sao: %d %d ", vetor[0], vetor[1]);
     
 }
